24ff22cd6e46cc61b32fc37b43ae53eea2f825d9
vcfgv <24791380+vcfgv@users.noreply.github.com>
2022-07-06 22:47:42 +0800

    [fish] update fish plugins and cursor

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -4,6 +4,11 @@ set -q XDG_CONFIG_HOME || set XDG_CONFIG_HOME "$HOME/.config"
 set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/zsh/themes/starship.toml
 
 set -x EDITOR nvim
+set fish_cursor_default block
+set fish_cursor_unknown line       blink
+set fish_cursor_insert  line       blink
+set fish_cursor_replace underscore blink
+set fish_cursor_visual  block
 set -x LS_COLORS "di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 set -x BAT_CONFIG_PATH $XDG_CONFIG_HOME/bat/config
 
